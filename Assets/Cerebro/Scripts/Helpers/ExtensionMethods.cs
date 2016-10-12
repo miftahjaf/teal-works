e5c296d3833a6ee670eae877a7e9449be0ac11d9
Ramiz Memon <ramiz@teal-labs.com>
2016-10-12 11:26:02 +0530

    Added extension methods class

---
+++ Assets/Cerebro/Scripts/Helpers/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ExtensionMethods
+{
+	private static System.Random rng = new System.Random();  
+	public static void Shuffle<T>(this T[] array)  
+	{  
+		int n = array.Length;  
+		while (n > 1) {  
+			n--;  
+			int k = rng.Next(n + 1);  
+			T value = array[k];  
+			array[k] = array[n];  
+			array[n] = value;  
+		}  
+	}
+
+}
