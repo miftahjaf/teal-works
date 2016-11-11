6060cf92c524c07fc986e6eca7f693ae6a39b94b
Basu Negi <vijaypal@teal-labs.com>
2016-11-11 11:32:05 +0530

    Added : New Chapter -> Fundamental Concepts of Geometry 7

---
+++ Assets/Cerebro/Scripts/Assessments/FundamentalConceptsOfGeometry7.cs
@@ -0,0 +1,431 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using MaterialUI;
+
+namespace Cerebro
+{
+	public class FundamentalConceptsOfGeometry7 : BaseAssessment
+	{
+		public Text subQuestionText;
+		public GameObject MCQ;
+		private string Answer;
+		private string[] expressions;
+		private int randSelector;
+		private List<string> options;
+
+		void Start()
+		{
+
+
+			StartCoroutine(StartAnimation());
+			base.Initialise("M", "FCG07", "S01", "A01");
+
+			scorestreaklvls = new int[1];
+			for (var i = 0; i < scorestreaklvls.Length; i++)
+			{
+				scorestreaklvls[i] = 0;
+			}
+
+			levelUp = false;
+
+
+			Answer = "";
+			GenerateQuestion();
+		}
+
+		public override void SubmitClick()
+		{
+			if (ignoreTouches || userAnswerLaText.text == "")
+			{
+				return;
+			}
+			int increment = 0;
+			ignoreTouches = true;
+			//Checking if the response was correct and computing question level
+			var correct = true;
+			CerebroHelper.DebugLog("!" + userAnswerLaText.text + "!");
+			CerebroHelper.DebugLog("*" + Answer + "*");
+			questionsAttempted++;
+			updateQuestionsAttempted();
+			Debug.Log ("submit");
+			if (MCQ.activeSelf) 
+			{
+				if (Answer == userAnswerLaText.text) 
+				{
+					correct = true;
+					StartCoroutine(AnimateMCQOption (Answer));
+				}
+				else
+				{
+					correct = false;
+					AnimateMCQOptionCorrect(Answer);
+
+				}
+			}
+			else
+			{
+				float answer = 0;
+				float userAnswer = 0;
+				bool directCheck = false;
+				if (float.TryParse(Answer, out answer))
+				{
+					answer = float.Parse(Answer);
+				}
+				else
+				{
+					directCheck = true;
+				}
+				if (float.TryParse(userAnswerLaText.text, out userAnswer))
+				{
+					userAnswer = float.Parse(userAnswerLaText.text);
+				}
+				else
+				{
+					correct = false;
+				}
+				if (answer != userAnswer)
+				{
+					correct = false;
+				}
+				if (directCheck)
+				{
+					if (userAnswerLaText.text == Answer)
+					{
+						correct = true;
+					}
+					else
+					{
+						correct = false;
+					}
+				}
+			}
+			if (correct == true)
+			{
+				if (Queslevel == 1)
+				{
+					UpdateStreak(10, 15);
+					increment = 5;
+				}
+				else if (Queslevel == 2)
+				{
+					UpdateStreak(10, 15);
+					increment = 10;
+				}
+
+				updateQuestionsAttempted();
+				StartCoroutine(ShowCorrectAnimation());
+			}
+			else
+			{
+				for (var i = 0; i < scorestreaklvls.Length; i++)
+				{
+					scorestreaklvls[i] = 0;
+				}
+				StartCoroutine(ShowWrongAnimation());
+			}
+
+			base.QuestionEnded(correct, level, increment, selector);
+
+		}
+
+		protected override IEnumerator ShowWrongAnimation()
+		{
+			userAnswerLaText.color = MaterialColor.red800;
+			Go.to(userAnswerLaText.gameObject.transform, 0.5f, new GoTweenConfig().shake(new Vector3(0, 0, 20), GoShakeType.Eulers));
+			yield return new WaitForSeconds(0.5f);
+			if (isRevisitedQuestion)
+			{
+				if (numPad.activeSelf)
+				{               // is not MCQ type question
+					userAnswerLaText.text = "";
+				}
+				if (userAnswerLaText != null)
+				{
+					userAnswerLaText.color = MaterialColor.textDark;
+				}
+				if (userAnswerLaText != null)
+				{
+					userAnswerLaText.color = MaterialColor.textDark;
+				}
+				ignoreTouches = false;
+			}
+			else
+			{
+				if (numPad.activeSelf)
+				{               // is not MCQ type question
+					CerebroHelper.DebugLog("going in if");
+					userAnswerLaText.text = Answer.ToString();
+					userAnswerLaText.color = MaterialColor.green800;
+				}
+				else
+				{
+					CerebroHelper.DebugLog("going in else");
+					userAnswerLaText.color = MaterialColor.textDark;
+				}
+			}
+
+			ShowContinueButton();
+		}
+
+		protected override IEnumerator ShowCorrectAnimation()
+		{
+			userAnswerLaText.color = MaterialColor.green800;
+			var config = new GoTweenConfig()
+				.scale(new Vector3(1.1f, 1.1f, 0))
+				.setIterations(2, GoLoopType.PingPong);
+			var flow = new GoTweenFlow(new GoTweenCollectionConfig().setIterations(1));
+			var tween = new GoTween(userAnswerLaText.gameObject.transform, 0.2f, config);
+			flow.insert(0f, tween);
+			flow.play();
+			yield return new WaitForSeconds(1f);
+			userAnswerLaText.color = MaterialColor.textDark;
+
+			showNextQuestion();
+
+			if (levelUp)
+			{
+				StartCoroutine(HideAnimation());
+				base.LevelUp();
+				yield return new WaitForSeconds(1.5f);
+				StartCoroutine(StartAnimation());
+			}
+
+		}
+
+		void AnimateMCQOptionCorrect(string ans)
+		{
+			for (int i = 1; i <= 4; i++) {
+				if (MCQ.transform.Find ("Option" + i.ToString ()).Find ("Text").GetComponent<TEXDraw> ().text == ans) {
+					MCQ.transform.Find ("Option" + i.ToString ()).Find ("Text").GetComponent<TEXDraw> ().color = MaterialColor.green800;
+				}
+			}
+		}
+
+		public void MCQOptionClicked (int value)
+		{
+			if (ignoreTouches) {
+				return;
+			}
+			userAnswerLaText = MCQ.transform.Find ("Option" + value.ToString ()).Find ("Text").GetComponent<TEXDraw> ();
+			//GeneralButton = MCQ.transform.Find ("Option" + value.ToString ()).GetComponent<Button> ();
+			SubmitClick ();
+		}
+
+		IEnumerator AnimateMCQOption (string ans)
+		{
+			var GO = new GameObject();
+			for (int i = 1; i <= 4; i++) {
+				if (MCQ.transform.Find ("Option" + i.ToString ()).Find ("Text").GetComponent<TEXDraw> ().text == ans) {
+					GO = MCQ.transform.Find ("Option" + i.ToString ()).gameObject;
+				}
+			}
+			Go.to (GO.transform, 0.2f, new GoTweenConfig ().scale (new Vector3 (1.2f, 1.2f, 1), false));
+			yield return new WaitForSeconds (0.2f);
+			Go.to (GO.transform, 0.2f, new GoTweenConfig ().scale (new Vector3 (1, 1, 1), false));
+		}
+
+		void RandomizeMCQOptionsAndFill(List<string> options)
+		{
+			int index = 0;
+			int cnt = options.Count;
+			for (int i = 1; i <= cnt; i++) {
+				index = Random.Range (0, options.Count);
+				MCQ.transform.Find ("Option"+i).Find ("Text").GetComponent<TEXDraw> ().text = options [index];
+				options.RemoveAt (index);
+			}
+		}
+
+		protected override void GenerateQuestion()
+		{
+			ignoreTouches = false;
+			base.QuestionStarted();
+			// Generating the parameters
+
+			level = Queslevel;
+
+			subQuestionText.gameObject.SetActive (false);
+			SetNumpadMode ();
+			options = new List<string> ();
+
+			for (int i = 1; i < 5; i++) {
+				MCQ.transform.Find ("Option" + i.ToString ()).Find ("Text").GetComponent<TEXDraw> ().color = MaterialColor.textDark;
+			}
+
+			if (Queslevel > scorestreaklvls.Length)
+			{
+				level = UnityEngine.Random.Range(1, scorestreaklvls.Length + 1);
+			}
+
+
+			#region level1
+			if (level == 1)
+			{
+				selector = GetRandomSelector(1, 7);
+
+				if (selector == 1)
+				{
+					SetMCQMode ();
+					QuestionText.text = "Which of the following units does not give the measure of a line segment?";
+
+					expressions = new string[] {"millilitre", "litre"};
+					options.Add (expressions [Random.Range (0, expressions.Length)]);
+					options.Add ("kilometer");
+					options.Add ("inch");
+					options.Add ("mile");
+
+					Answer = options [0];
+					RandomizeMCQOptionsAndFill (options);
+				}
+				else if (selector == 2)
+				{
+					subQuestionText.gameObject.SetActive (true);
+					SetMCQMode (2);
+
+					QuestionText.text = "State True or False :";
+					expressions = new string[] {"FA ray has a finite length.",
+												"TThere are infinitely many lines on a plane.",
+												"FA line has two end points.",
+												"FThe lines in a plane always intersect."};
+					
+					randSelector = Random.Range (0, expressions.Length);
+					subQuestionText.text = expressions[randSelector].Substring (1);
+					Answer = expressions[randSelector][0] == 'T'? "True": "False";
+
+					MCQ.transform.Find ("Option1").Find ("Text").GetComponent<TEXDraw> ().text = "True";
+					MCQ.transform.Find ("Option2").Find ("Text").GetComponent<TEXDraw> ().text = "False";
+				}
+				else if (selector == 3)
+				{
+					randSelector = Random.Range (1, 3);
+					expressions = new string[] {"∞How many lines can be drawn through a point?",
+												"1How many lines can be drawn through two fixed points?",
+												"3How many lines can be drawn through three non-collinear points?"};
+
+					randSelector = Random.Range (0, expressions.Length);
+					QuestionText.text = expressions[randSelector].Substring (1);
+					Answer = expressions[randSelector][0].ToString();
+				}
+				else if (selector == 4)
+				{
+					expressions = new string[] {string.Format ("∞A line segment AB has length {0} cm. How many points are there on this line segment", Random.Range (2, 10)),
+												string.Format ("{0}How many {1} points of intersection can three lines have?", randSelector == 1? 3 : 0, randSelector == 1? "maximum" : "minimum"),
+												string.Format ("8Mark any five points A, B, C, D and E such that A, B and D are collinear and rest are not collinear. How many lines can be drawn through these points?")};
+
+					randSelector = Random.Range (0, expressions.Length);
+					QuestionText.text = expressions[randSelector].Substring (1);
+					Answer = expressions[randSelector][0].ToString();
+				}
+				else if (selector == 5)
+				{
+					randSelector = Random.Range (4, 7);
+
+					QuestionText.text = string.Format ("How many line segments can be drawn through {0} non-collinear points?", randSelector);
+					Answer = "" + randSelector * (randSelector - 1) / 2;
+				}
+				else if (selector == 6)
+				{
+					SetMCQMode (2);
+					randSelector = Random.Range (4, 7);
+
+					QuestionText.text = "Three lines P, Q and R are concurrent and lines R, S and T are also concurrent. Is it necessary that lines P, Q and S will be concurrent?";
+					Answer = "No";
+
+					MCQ.transform.Find ("Option1").Find ("Text").GetComponent<TEXDraw> ().text = "Yes";
+					MCQ.transform.Find ("Option2").Find ("Text").GetComponent<TEXDraw> ().text = "No";
+				}
+			}
+			#endregion
+			userAnswerLaText = GeneralButton.gameObject.GetChildByName<TEXDraw>("Text");
+			userAnswerLaText.text = "";
+		}
+
+		public override void numPadButtonPressed(int value)
+		{
+			if (ignoreTouches)
+			{
+				return;
+			}
+			if (value <= 9)
+			{
+				userAnswerLaText.text += value.ToString();
+			}
+			else if (value == 10)
+			{    //∞
+				if (checkLastTextFor(new string[1] { "∞" }))
+				{
+					userAnswerLaText.text = userAnswerLaText.text.Substring(0, userAnswerLaText.text.Length - 1);
+				}
+				userAnswerLaText.text += "∞";
+			}
+			else if (value == 11)
+			{   // Back
+				if (userAnswerLaText.text.Length > 0)
+				{
+					userAnswerLaText.text = userAnswerLaText.text.Substring(0, userAnswerLaText.text.Length - 1);
+				}
+			}
+			else if (value == 12) {   // All Clear
+				userAnswerLaText.text = "";
+			}
+		}
+		protected void SetMCQMode (int NumberOfMCQ = 4)
+		{
+			this.MCQ.SetActive (true);
+			Vector2[] positions;
+
+			if (NumberOfMCQ == 3) 
+			{
+				positions = new Vector2[] {
+					new Vector2 (-255, 40f),
+					new Vector2 (0, 40f),
+					new Vector2 (255, 40f),
+					new Vector2 (0, 0f)
+				};	
+			} 
+			else if (NumberOfMCQ == 2) {
+				positions = new Vector2[] {
+					new Vector2 (-180, 40f),
+					new Vector2 (180, 40f),
+					new Vector2 (-180, 0f),
+					new Vector2 (180, 0f)
+				};
+			}
+			else 
+			{
+				positions = new Vector2[] {
+					new Vector2 (-180, 80f),
+					new Vector2 (180, 80f),
+					new Vector2 (-180, 0f),
+					new Vector2 (180, 0f)
+				};
+			}
+
+			for (int i = 1; i <= 4; i++)
+			{
+				MCQ.transform.Find ("Option" + i).gameObject.SetActive (i<=NumberOfMCQ);
+				MCQ.transform.Find ("Option" + i).GetComponent<RectTransform> ().anchoredPosition = positions[i-1];
+			}
+			this.MCQ.SetActive (true);
+			this.numPad.SetActive (false);
+			this.GeneralButton.gameObject.SetActive (false);
+
+			for (int i = 1; i <= 4; i++) {
+				if(MCQ.transform.Find ("Option" + i.ToString ()).Find ("Text").GetComponent<TEXDraw> ())
+					MCQ.transform.Find ("Option" + i.ToString ()).Find ("Text").GetComponent<TEXDraw> ().color = MaterialColor.textDark;
+				else
+					MCQ.transform.Find ("Option" + i.ToString ()).Find ("Text").GetComponent<Text> ().color = MaterialColor.textDark;
+			}
+
+
+		}
+		protected void SetNumpadMode ()
+		{
+			this.numPad.SetActive (true);
+			this.MCQ.SetActive (false);
+			this.GeneralButton.gameObject.SetActive (true);
+		}
+	}
+}
+
