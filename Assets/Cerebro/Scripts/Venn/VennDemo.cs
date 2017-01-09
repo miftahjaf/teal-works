7ec7351c0ade147d42fb51a2a4d7b0b97bfb6c4d
Ramiz <ramiz@Ramizs-MacBook-Air.local>
2017-01-09 15:26:40 +0530

    Added: Venn Helper

---
+++ Assets/Cerebro/Scripts/Venn/VennDemo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cerebro
+{
+	public class VennDemo : MonoBehaviour
+	{
+		public VennHelper venHelper;
+
+		public void Start()
+		{
+			venHelper.SetDiagramValues (new List<string> (){"8 9 10", "1\n 3 \n 5 ","0 \n 2 \n7","8 9 5" });
+			//venHelper.SetIsInteractable (true);
+			venHelper.SetFillValue(0);
+			venHelper.SetFillValue(3);
+			venHelper.SetVennType (VennType.Overlapping);
+			venHelper.Draw ();
+		}
+
+	}
+}
---
+++ Assets/Cerebro/Scripts/Venn/VennHelper.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+namespace Cerebro
+{
+	public enum VennType
+	{
+		None = -1,
+		Overlapping =0,
+		Disjoint = 1,
+		Inside = 2
+
+	}
+	public class VennHelper : MonoBehaviour 
+	{
+		public GameObject[] vennDiagramPrefabs;
+		private VennType vennType;
+		private List<string> diagramValues; //rect, left, middle, right
+		private GameObject diagramObj;
+		private bool isInteractable;
+		private bool[] filledValues = new bool[]{false,false,false,false};
+
+		public void Reset()
+		{
+			//Destroy child
+			foreach (Transform child in transform) 
+			{
+				GameObject.Destroy(child.gameObject);
+			}
+
+			diagramValues = new List<string> ();
+			vennType = VennType.None;
+			isInteractable = false;
+			diagramObj = null;
+			filledValues = new bool[]{false,false,false,false};
+		}
+
+		public void Draw()
+		{
+			GenerateDiagram ();
+		}
+
+		public void SetVennType(VennType _vennType)
+		{
+			vennType = _vennType;
+		}
+
+		public void SetDiagramValues(List<string> _diagramValues)
+		{
+			diagramValues = _diagramValues;
+		}
+
+		public void SetIsInteractable(bool _isInteractable)
+		{
+			isInteractable = _isInteractable;
+		}
+
+		public void SetFillValue(int index)
+		{
+			filledValues [index] = true;
+		}
+
+		public void GenerateDiagram()
+		{
+			int number = (int)vennType;
+			if (number < 0) {
+				return;
+			}
+			diagramObj = GameObject.Instantiate (vennDiagramPrefabs [number]);
+			diagramObj.transform.SetParent (this.transform, false);
+			int count = diagramObj.transform.childCount;
+			foreach (UIPolygon uiPolygon in diagramObj.GetComponentsInChildren<UIPolygon>()) {
+				uiPolygon.ReDraw ();
+			}
+			for(int i=0; i<count; i++)
+			{
+				int value = i;
+				GameObject childObj = diagramObj.transform.FindChild (value.ToString ()).gameObject;
+
+				if (diagramValues.Count > i) 
+				{
+					childObj.transform.FindChild ("Text").GetComponent<Text> ().text = diagramValues [i];
+				}
+
+				if (filledValues [i]) 
+				{
+					if (childObj.GetComponent<UIPolygon> ()) 
+					{
+						childObj.transform.FindChild ("Fill").GetComponent<UIPolygon> ().color = Color.blue;
+					} else {
+						childObj.GetComponent<Graphic> ().color =  Color.blue;
+					}
+				}
+
+				if (isInteractable)
+				{
+					childObj.GetComponent<ColliderButton> ().OnClicked = delegate {
+						bool isSelected = false;
+						if (childObj.GetComponent<UIPolygon> ()) {
+							isSelected = childObj.transform.FindChild("Fill").GetComponent<UIPolygon> ().color == Color.blue;
+							childObj.transform.FindChild("Fill").GetComponent<UIPolygon> ().color = isSelected ? Color.white : Color.blue;
+							childObj.transform.FindChild("Fill").GetComponent<UIPolygon> ().ReDraw();
+						} else {
+							isSelected = childObj.GetComponent<Graphic> ().color == Color.blue;
+							childObj.GetComponent<Graphic> ().color = isSelected ? Color.white : Color.blue;
+						}
+					};
+				}
+
+			}
+		}
+	}
+}
